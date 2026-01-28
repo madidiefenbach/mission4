163aa655ef20fc6ef5f02e88428bd956466353f6
madidiefenbach <madidiefenbach@gmail.com>
2026-01-28 13:56:01 -0700

    better

--- Program.cs
+++ Program.cs
@@ -2,26 +2,35 @@
 // section 3 group 13
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
+gameEngine ge = new gameEngine();
 
 
-string [] boardGame = [];
-string choice = "";
-string choice2 = "";
+int [] boardGame = new int[9];
+int choice = 0;
+int choice2 = 0;
+bool gameOver = false;
+string currentPlayer = "X";
 
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
+for (int spot = 0; spot < boardGame.Length; spot++)
+{
+    boardGame[spot] = choice.ToString();
+}
 
 Console.WriteLine("Player 1: Choose a spot (0-8");
 choice = Console.ReadLine();
 Console.WriteLine("Player 2: Choose a spot (0-8");
 choice2 = Console.ReadLine();
 
-do
+while (!gameOver)
 {
+    ge.printBoard(boardGame);
     Console.WriteLine("Player 1: Choose a spot (0-8");
     choice = Console.ReadLine();
-    boardGame[choice] = choice;
+    boardGame[choice] = 1;
+
+    ge.printBoard(boardGame);
     Console.WriteLine("Player 2: Choose a spot (0-8");
     choice2 = Console.ReadLine();
-    boardGame[choice2] = choice2;
+    boardGame[choice2] = 2;
 }
-while (boardGame.Length <= 8);
