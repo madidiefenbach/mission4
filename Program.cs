98f81dc7c0966e908c1ee113424f6323ba48fffc
madidiefenbach <madidiefenbach@gmail.com>
2026-01-28 14:14:14 -0700

    running

--- Program.cs
+++ Program.cs
@@ -1,31 +1,42 @@
 ﻿//Madison Diefenbach
 // section 3 group 13
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.Design;
-gameEngine ge = new gameEngine();
+//Madison Diefenbach
+// section 3 group 13
+//Madison Diefenbach
+// section 3 group 13
+namespace mission4;
 
-
-int [] boardGame = new int[9];
-int choice = 0;
-int choice2 = 0;
-bool gameOver = false;
-
-
-Console.WriteLine("Welcome to Tic-Tac-Toe!");
-for (int spot = 0; spot < boardGame.Length; spot++)
+public class Program
 {
-    boardGame[spot] = choice;
-}
+    static void Main()
+    {
+        gameEngine ge = new gameEngine();
 
-while (!gameOver)
-{
-    ge.printBoard(boardGame);
-    Console.WriteLine("Player 1: Choose a spot (0-8");
-    choice = Console.ReadLine();
-    boardGame[choice] = 1;
+        int[] boardGame = new int[9];
+        int choice;
+        int choice2;
+        bool gameOver = false;
 
-    ge.printBoard(boardGame);
-    Console.WriteLine("Player 2: Choose a spot (0-8");
-    choice2 = Console.ReadLine();
-    boardGame[choice2] = 2;
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+        for (int spot = 0; spot < boardGame.Length; spot++)
+        {
+            boardGame[spot] = spot;
+        }
+
+        while (!gameOver)
+        {
+            ge.printBoard(boardGame);
+
+            Console.WriteLine("Player 1: Choose a spot (0-8)");
+            choice = int.Parse(Console.ReadLine());
+            boardGame[choice] = 1;
+
+            ge.printBoard(boardGame);
+
+            Console.WriteLine("Player 2: Choose a spot (0-8)");
+            choice2 = int.Parse(Console.ReadLine());
+            boardGame[choice2] = 2;
+        }
+    }
 }
