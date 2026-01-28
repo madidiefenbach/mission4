a119e6ba95dbd57d2188d3ce5d082c2fada82174
madidiefenbach <madidiefenbach@gmail.com>
2026-01-28 14:03:06 -0700

    stuff

--- Program.cs
+++ Program.cs
@@ -9,28 +9,23 @@ int [] boardGame = new int[9];
 int choice = 0;
 int choice2 = 0;
 bool gameOver = false;
-string currentPlayer = "X";
+
 
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
 for (int spot = 0; spot < boardGame.Length; spot++)
 {
-    boardGame[spot] = choice.ToString();
+    boardGame[spot] = choice;
 }
-
-Console.WriteLine("Player 1: Choose a spot (0-8");
-choice = Console.ReadLine();
-Console.WriteLine("Player 2: Choose a spot (0-8");
-choice2 = Console.ReadLine();
 
 while (!gameOver)
 {
     ge.printBoard(boardGame);
     Console.WriteLine("Player 1: Choose a spot (0-8");
-    choice = Console.ReadLine();
+    choice = int.Parse(Console.ReadLine());
     boardGame[choice] = 1;
 
     ge.printBoard(boardGame);
     Console.WriteLine("Player 2: Choose a spot (0-8");
-    choice2 = Console.ReadLine();
+    choice2 = int.Parse(Console.ReadLine());
     boardGame[choice2] = 2;
 }
